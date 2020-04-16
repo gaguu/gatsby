3e8c1bbf1c618d372c557a7288a30619706e417b
gaguu <gaggudd79@gmail.com>
2020-04-16 22:39:09 +0530

    uaosdkas

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/src/assets/img`,
         name: 'uploads',
       },
     },
